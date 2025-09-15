bb4e6faf4f80c8ba350efcb445e20d84cdab010c
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2025-09-15 18:10:48 +0530

    third  commmit

--- src/test/java/com/pm/springjenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/pm/springjenkins/api/SpringJenkinsApplicationTests.java
@@ -14,7 +14,7 @@ class SpringJenkinsApplicationTests {
     public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
     @Test
-    void contextLoads() {
+    public  contextLoads() {
         logger.info("Text case executing......");
         assertEquals(true, true);
     }
